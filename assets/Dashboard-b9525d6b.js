48a6bf33ffddfd658dfabd36fe9f754ff22801a7
JulioSergioFS <juliosergio010302@gmail.com>
2023-02-03 16:34:48 -0300

    Updates

--- assets/Dashboard-b9525d6b.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as s,a as r}from"./index-a3968ede.js";function d(){return a.useState(0),s("div",{children:[r("div",{children:"sidebar"}),r("div",{children:"content"})]})}export{d as default};
