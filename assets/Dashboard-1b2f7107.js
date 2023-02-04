797805d545c3a1b8628107e69a7b1435888b0fb6
JulioSergioFS <juliosergio010302@gmail.com>
2023-02-04 14:34:35 -0300

    Updates

--- assets/Dashboard-1b2f7107.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as s,a as r}from"./index-83003ea1.js";function d(){return a.useState(0),s("div",{children:[r("div",{children:"sidebar"}),r("div",{children:"content"})]})}export{d as default};
--- assets/Ecommerce-c4011f36.js
+++
@@ -1,1 +0,0 @@
-import{r,a as e}from"./index-83003ea1.js";function a(){return r.useState(0),e("div",{children:"a"})}export{a as default};
