1a8f6a05c8e6f7a93c5d799dde145af2eb132482
JulioSergioFS <juliosergio010302@gmail.com>
2023-03-10 18:22:04 -0300

    Updates

---
+++ assets/Charts-e2fc0d12.js
@@ -0,0 +1,1 @@
+import{y as t,Y as a,$ as r}from"./index-8e69b818.js";function e(){return t.useState(0),a(r,{title:"Charts",children:"charts"})}export{e as default};
--- assets/Ecommerce-109fa115.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as r,P as t}from"./index-a667939c.js";function c(){return e.useState(0),r(t,{title:"Ecommerce",children:"a"})}export{c as default};
