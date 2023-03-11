0f2004aeaee9cecadb817ead5fda28eddb357c8c
JulioSergioFS <juliosergio010302@gmail.com>
2023-03-10 22:43:56 -0300

    add  animation and responsive to sidebar and dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
+import hamburgerMd from "@iconify/icons-ci/hamburger-md";
+import arrowBackIosRounded from "@iconify/icons-material-symbols/arrow-back-ios-rounded";
 import logoutVariant from "@iconify/icons-mdi/logout-variant";
 import userIcon from "@iconify/icons-mdi/user";
 import { Icon } from "@iconify/react";
-import { m } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence, m } from "framer-motion";
+import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { Page } from "../components/Page";
 import { UserAvatar } from "../components/UserAvatar";
@@ -10,6 +12,8 @@ import { pages } from "../constants/sidebarPages";
 import "../styles/dashboard.scss";
 
 export default function Dashboard() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 820);
+  const [showSidebar, setShowSidebar] = useState(isMobile);
   const [showProfilePopup, setShowProfilePopup] = useState(false);
   const navigate = useNavigate();
 
@@ -19,74 +23,113 @@ export default function Dashboard() {
     localStorage.removeItem("name");
   };
 
+  useEffect(() => {
+    window.onresize = () => setIsMobile(window.innerWidth <= 820);
+  }, [window.innerWidth]);
+
   return (
     <Page title="Dashboard">
       <main>
-        <div className="sidebar">
-          <m.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.08 }}
-            className="logo"
-          >
-            logo
-          </m.div>
-          <m.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.1 }}
-            className="user-card"
-          >
-            <UserAvatar />
-            <div className="user-card-data">
-              <p className="user-name">{localStorage.getItem("name")}</p>
-              <p className="user-role">
-                {localStorage.getItem("email") === "admin@gmail.com"
-                  ? "administrador"
-                  : "visitante"}
-              </p>
-            </div>
-          </m.div>
-          <ul className="page-list">
-            {pages.map((page, index) => (
-              <m.li
+        <AnimatePresence>
+          {showSidebar || !isMobile ? (
+            <m.div
+              key="sidebar"
+              initial={{ x: -200 }}
+              animate={{ x: 0 }}
+              exit={{ x: -400 }}
+              transition={{ duration: 0.3 }}
+              className="sidebar"
+            >
+              <m.div
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.7 * ((index + 1) / 6) }}
-                key={page.name}
-                className="list-item"
-                onClick={() => navigate(`${page.link}`)}
+                transition={{ duration: 0.08 }}
+                className="sidebar_first-element"
               >
-                {page.icon}
-                {page.name}
-              </m.li>
-            ))}
-          </ul>
-        </div>
+                <div className="logo">logo</div>
+
+                {isMobile ? (
+                  <Icon
+                    icon={arrowBackIosRounded}
+                    height={24}
+                    className="clickable"
+                    onClick={() => setShowSidebar(false)}
+                  />
+                ) : null}
+              </m.div>
+
+              <m.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.1 }}
+                className="user-card"
+              >
+                <UserAvatar />
+                <div className="user-card-data">
+                  <p className="user-name">{localStorage.getItem("name")}</p>
+                  <p className="user-role">
+                    {localStorage.getItem("email") === "admin@gmail.com"
+                      ? "administrador"
+                      : "visitante"}
+                  </p>
+                </div>
+              </m.div>
+              <ul className="page-list">
+                {pages.map((page, index) => (
+                  <m.li
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.7 * ((index + 1) / 6) }}
+                    key={page.name}
+                    className="list-item"
+                    onClick={() => navigate(`${page.link}`)}
+                  >
+                    {page.icon}
+                    {page.name}
+                  </m.li>
+                ))}
+              </ul>
+            </m.div>
+          ) : null}
+        </AnimatePresence>
         <div className="content">
           <header className="header">
+            {isMobile ? (
+              <Icon
+                icon={hamburgerMd}
+                height={40}
+                className="clickable"
+                onClick={() => setShowSidebar(true)}
+              />
+            ) : null}
             <UserAvatar
               className="clickable"
               onClick={() => setShowProfilePopup(!showProfilePopup)}
             />
-            {showProfilePopup ? (
-              <m.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
-                exit={{ opacity: 0 }}
-                className="account-popup"
-              >
-                <div className="list-item" onClick={() => navigate("profile")}>
-                  <Icon icon={userIcon} height={22} />
-                  Editar Perfil
-                </div>
-                <div className="list-item" onClick={handleLogout}>
-                  <Icon icon={logoutVariant} height={22} />
-                  Sair
-                </div>
-              </m.div>
-            ) : null}
+            <AnimatePresence>
+              {showProfilePopup ? (
+                <m.div
+                  key="popup"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.3 }}
+                  exit={{ opacity: 0 }}
+                  className="account-popup"
+                >
+                  <div
+                    className="list-item"
+                    onClick={() => navigate("profile")}
+                  >
+                    <Icon icon={userIcon} height={22} />
+                    Editar Perfil
+                  </div>
+                  <div className="list-item" onClick={handleLogout}>
+                    <Icon icon={logoutVariant} height={22} />
+                    Sair
+                  </div>
+                </m.div>
+              ) : null}
+            </AnimatePresence>
           </header>
           <section>
             <Outlet />
